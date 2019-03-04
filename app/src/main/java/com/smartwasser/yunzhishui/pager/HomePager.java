265772bf42f3099cb1ca1dc470efecc33b8f75ca
cjn <caijiannan134@163.com>
2019-03-04 14:23:09 +0800

    表格4

--- app/src/main/java/com/smartwasser/yunzhishui/pager/HomePager.java
+++ app/src/main/java/com/smartwasser/yunzhishui/pager/HomePager.java
@@ -35,10 +35,10 @@ public class HomePager extends BasePager implements SwipeRefreshLayout.OnRefresh
     private SwipeRefreshLayout downRefresh;
     private RecyclerView recyclerView;
     private MyAdapter adapter;
-    private String[] mText ={"远程监控","预报预警","汇总统计","泵站监测","历史查询"};
+    private String[] mText ={"远程监控","预报预警","汇总统计","泵站监测","历史查询","数据分析","远程控制"};
 //    private String[] mText ={"生产数据","生产成本","远程监控","设备运行","污泥运输","生产指标","采购","防汛信息","工艺预测","远程调控"};
 //    private int[] icon={R.drawable.home_11,R.drawable.home_22,R.drawable.home_33,R.drawable.home_44,R.drawable.home_55,R.drawable.home_66,R.drawable.home_77,R.drawable.home_99,R.drawable.home_88,R.drawable.home_10};
-    private int[] icon={R.drawable.home_33,R.drawable.home_11,R.drawable.home_22,R.drawable.home_44,R.drawable.home_66};
+    private int[] icon={R.drawable.home_33,R.drawable.home_11,R.drawable.home_22,R.drawable.home_44,R.drawable.home_66,R.drawable.home_77,R.drawable.home_88};
 
     public HomePager(Context context) {
         super(context);
@@ -130,6 +130,7 @@ public class HomePager extends BasePager implements SwipeRefreshLayout.OnRefresh
                                 break;
                             case 1:
                                 /**跳到成本管理*/
+
 //                                Intent intent2=new Intent(context, ProductionActivity.class);
                                 Intent intent2=new Intent(context, AlarmActivity.class);
                                 context.startActivity(intent2);
